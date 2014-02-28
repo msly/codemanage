b155ab0513fe8f136756220db8a8711318129546
msly <msly.2008@163.com>
2014-02-28 22:56:15 +0800

    GetLocalTime

--- MFC消息类.cpp
+++ MFC消息类.cpp
@@ -14,3 +14,28 @@ else if( uMsg == WM_NCPAINT )
 {            
 	return 0;
 }
+
+MFC时间操作
+#define _SECOND ((ULONGLONG) 10000000)
+#define _MINUTE (60 * _SECOND)
+#define _HOUR   (60 * _MINUTE)
+#define _DAY    (24 * _HOUR)
+
+void CTestClass::ChangeTime(SYSTEMTIME& sysTime)
+{ 
+    GetLocalTime(&sysTime);
+
+    FILETIME ft={0}; 
+    SystemTimeToFileTime(&sysTime, &ft); 
+
+    ULONGLONG qwResult;
+    // Copy the time into a quadword.
+    qwResult = (((ULONGLONG) ft.dwHighDateTime) << 32) + ft.dwLowDateTime;
+    // minus 2 hours.
+    qwResult -= 2 * _HOUR;
+    // Copy the result back into the FILETIME structure.
+    ft.dwLowDateTime  = (DWORD) (qwResult & 0xFFFFFFFF );
+    ft.dwHighDateTime = (DWORD) (qwResult >> 32 );
+
+    FileTimeToSystemTime(&ft, &sysTime);
+} 
